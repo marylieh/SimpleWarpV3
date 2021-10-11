22bc089a6d9d6aa9e5b654a1eb20da31b8d14c7e
asmax15 <64687392+asmax15@users.noreply.github.com>
2021-10-11 19:45:53 +0200

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "spigot-gradle-kotlin-template"
---
+++ src/main/kotlin/ExamplePlugin.kt
@@ -0,0 +1,9 @@
+package com.example.exampleplugin
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class ExamplePlugin : JavaPlugin() {
+
+    override fun onEnable() { }
+
+}
