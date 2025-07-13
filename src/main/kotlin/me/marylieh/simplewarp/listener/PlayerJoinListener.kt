3e7cdb1e1f42801797fc5a2d59fe9546d039871e
marylieh <m.aschmann@icloud.com>
2025-07-13 15:01:13 +0200

    Add debug output

--- src/main/kotlin/me/marylieh/simplewarp/listener/PlayerJoinListener.kt
+++ src/main/kotlin/me/marylieh/simplewarp/listener/PlayerJoinListener.kt
@@ -3,15 +3,18 @@ package me.marylieh.simplewarp.listener
 import me.marylieh.simplewarp.permissions.PermissionManager
 import me.marylieh.simplewarp.utils.Config
 import me.marylieh.simplewarp.utils.PermissionFile
+import org.bukkit.Bukkit
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
+import java.util.logging.Level
 
 class PlayerJoinListener : Listener {
 
     @EventHandler fun handleJoin(event: PlayerJoinEvent) {
         @Suppress("UNCHECKED_CAST")
         val permissions = PermissionFile.getFile().getList("DefaultPermissions") as List<String>
+        Bukkit.getLogger().log(Level.INFO, "Got default Permissions: $permissions")
 
         if (Config.getConfig().getBoolean("IntegratedPermissionSystem")) {
             val integratedPermissions = PermissionManager.getPermissions(event.player)
@@ -22,6 +25,7 @@ class PlayerJoinListener : Listener {
                 }
 
                 PermissionManager.setPermission(event.player, it, true)
+                Bukkit.getLogger().log(Level.INFO, "Setting $it for ${event.player.name} from Integrated Permissions")
             }
         }
 
@@ -32,6 +36,7 @@ class PlayerJoinListener : Listener {
             }
 
             PermissionManager.setPermission(event.player, it, true)
+            Bukkit.getLogger().log(Level.INFO, "Setting $it for ${event.player.name} from Default Permissions")
         }
 
     }
